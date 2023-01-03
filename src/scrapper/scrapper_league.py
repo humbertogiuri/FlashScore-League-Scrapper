1f57ce17f3d0fafa44d9f5d39bce9fdfa2045daa
Humberto Giuri Calente <humbertogiuri@gmail.com>
2023-01-03 16:45:48 -0300

    getting stats from a single match

--- src/scrapper/scrapper_league.py
+++ src/scrapper/scrapper_league.py
@@ -30,10 +30,51 @@ def open_all_matchs(driver):
     except TimeoutException:
         print('End of expand matchs...')
 
+
 def get_all_id_matchs(driver):
     print('Getting all IDs matchs')
 
     return driver.find_elements(By.CLASS_NAME, 'event__match.event__match--static.event__match--twoLine')
+
+
+def get_match_stats(id_match):
+    print(f'Scrapping Match -> {id_match}')
+
+    match_stats = dict()
+
+    options = Options()
+    options.headless
+    options.add_argument('--disable-blink-features=AutomationControlled')
+
+    service = ChromeService(executable_path=ChromeDriverManager().install())
+
+    driver = webdriver.Chrome(service=service)
+    driver.maximize_window()
+
+    driver.get(f'https://www.flashscore.com/match/{id_match}/#/match-summary/match-statistics/0')
+    accept_cookies(driver)
+    
+    tournament_header = WebDriverWait(driver, 1).until(
+        EC.visibility_of_element_located((By.CLASS_NAME, 'tournamentHeader__country')))
+    
+    match_round_element = tournament_header.find_element(By.TAG_NAME, 'a')
+    match_stats['Round'] = match_round_element.text.split('ROUND ')[-1]
+    
+    #Getting All Stats
+    all_stats = driver.find_elements(By.CLASS_NAME, 'stat__category')
+
+    #Get Values for each stat (home and away)
+    for stat in all_stats:
+        stat_name = stat.find_element(By.CLASS_NAME, 'stat__categoryName')
+        home_stat = stat.find_element(By.CLASS_NAME, 'stat__homeValue')
+        away_stat = stat.find_element(By.CLASS_NAME, 'stat__awayValue')
+
+        match_stats[f'Home {stat_name.text}'] = home_stat.text
+        match_stats[f'Away {stat_name.text}'] = away_stat.text
+    
+    driver.quit()
+
+    return match_stats
 
 
 if __name__ == '__main__':
@@ -62,6 +103,7 @@ if __name__ == '__main__':
         open_all_matchs(driver)
         matchs_elements = get_all_id_matchs(driver)
         matchs_ids = [x.get_attribute('id').split('_')[-1] for x in matchs_elements]
+        match_stats = get_match_stats(matchs_ids[0])
         
         driver.quit()
         
